07e029bc7ad62603d174b028d8e530b7093bd916
ziaaurehman <ziaareact@gmail.com>
2025-02-18 02:33:46 +0500

    android setup

---
+++ android/app/.cxx/Debug/733tp4u2/prefab/x86/prefab/lib/i686-linux-android/cmake/hermes-engine/hermes-engineConfig.cmake
@@ -0,0 +1,9 @@
+if(NOT TARGET hermes-engine::libhermes)
+add_library(hermes-engine::libhermes SHARED IMPORTED)
+set_target_properties(hermes-engine::libhermes PROPERTIES
+    IMPORTED_LOCATION "/Users/macbook/.gradle/caches/8.9/transforms/ab634feb0febb1555ef179da09133465/transformed/hermes-android-0.77.1-debug/prefab/modules/libhermes/libs/android.x86/libhermes.so"
+    INTERFACE_INCLUDE_DIRECTORIES "/Users/macbook/.gradle/caches/8.9/transforms/ab634feb0febb1555ef179da09133465/transformed/hermes-android-0.77.1-debug/prefab/modules/libhermes/include"
+    INTERFACE_LINK_LIBRARIES ""
+)
+endif()
+
---
+++ android/app/.cxx/Debug/733tp4u2/prefab/x86_64/prefab/lib/x86_64-linux-android/cmake/fbjni/fbjniConfig.cmake
@@ -0,0 +1,9 @@
+if(NOT TARGET fbjni::fbjni)
+add_library(fbjni::fbjni SHARED IMPORTED)
+set_target_properties(fbjni::fbjni PROPERTIES
+    IMPORTED_LOCATION "/Users/macbook/.gradle/caches/8.9/transforms/45260c2e75af281db00e0d2660535977/transformed/fbjni-0.7.0/prefab/modules/fbjni/libs/android.x86_64/libfbjni.so"
+    INTERFACE_INCLUDE_DIRECTORIES "/Users/macbook/.gradle/caches/8.9/transforms/45260c2e75af281db00e0d2660535977/transformed/fbjni-0.7.0/prefab/modules/fbjni/include"
+    INTERFACE_LINK_LIBRARIES ""
+)
+endif()
+
